a8139e61b54533d81b7eb69555bd95ec3ae94369
Jason Laster <jason.laster.11@gmail.com>
2017-01-09 09:36:08 -0500

    readme

---
+++ README.md
@@ -0,0 +1,5 @@
+### Mixer
+
+This repo keeps the [calls](./calls.js) script and schedule.
+
+The sheet for the group is maintained [here](https://docs.google.com/spreadsheets/d/1j0smVu6HX1EUn4IeUaJc5uGgPhq3wcNANvbEcZR4aIg/edit#gid=0)
---
+++ calls.js
@@ -0,0 +1,81 @@
+// using SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+var helper = require('sendgrid').mail
+const yaml = require('yamljs')
+
+function getEmail(name) {
+  const email = people[name];
+  return new helper.Email(email, name)
+}
+
+function createEmail(from, first, second) {
+
+  let mail = new helper.Mail()
+  mail.setFrom(getEmail(from))
+  mail.setSubject(subject)
+
+  const content = new helper.Content(
+    "text/plain",
+    getBody(first, second)
+  )
+
+  mail.addContent(content)
+
+  personalization = new helper.Personalization()
+  personalization.addTo(getEmail(first))
+
+  if (second) {
+    personalization.addTo(getEmail(second))
+  }
+
+  mail.addPersonalization(personalization)
+
+  return mail
+}
+
+function sendEmail(from, first, second) {
+  const mail = createEmail(from, first, second)
+  const sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+  const request = sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: mail.toJSON()
+  });
+
+  sg.API(request, function(error, response) {
+    console.log(response.statusCode, response.body, response.headers)
+    if (error) {
+      console.log(error)
+    }
+  })
+}
+
+
+const subject = `Weekly Mixer`;
+
+function getBody(first, second) {
+
+  if (!second) {
+    const firstLine = `Hey ${first}, this week you have not been paired with anyone.`
+    const secondLine = `Feel free to treat this week as a wild card week and schedule a call with anyone you would like.`
+    return `${firstLine}\n\n${secondLine}`;
+  }
+
+  const firstLine = `Hey ${first} ${second},`;
+  const secondLine = `You've been paired this week! Feel free to reach out and schedule a time to talk.`;
+  return `${firstLine}\n\n${secondLine}`
+}
+
+
+const { people, schedule } = yaml.load("schedule.yaml");
+const week = schedule["week 2"]
+
+week.forEach(pair => {
+  if (pair.includes("\t")) {
+    const [first, second] = pair.split("\t")
+    sendEmail("Jason", first, second)
+  } else {
+    sendEmail("Jason", pair)
+
+  }
+})
