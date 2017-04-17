5767afcb9b74f1fc3363ce11506010381c30d37c
Jason Laster <jason.laster.11@gmail.com>
2017-04-17 09:48:27 -0400

    switch to shuffling

--- calls.js
+++ calls.js
@@ -1,78 +1,75 @@
 // using SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
-var helper = require('sendgrid').mail
-const yaml = require('yamljs')
+var helper = require('sendgrid').mail;
+const yaml = require('yamljs');
+const zip = require('lodash/zip');
+const {people} = yaml.load('schedule.yaml');
+const shuffle = require('shuffle-array');
 
 function getEmail(name) {
   const email = people[name];
-  return new helper.Email(email, name)
+  return new helper.Email(email, name);
 }
 
 function createEmail(from, first, second) {
-  let mail = new helper.Mail()
-  mail.setFrom(getEmail(from))
-  mail.setSubject(subject)
+  console.log(`email: ${first} ${second}`);
+  let mail = new helper.Mail();
+  mail.setFrom(getEmail(from));
+  mail.setSubject(subject);
 
-  const content = new helper.Content(
-    "text/plain",
-    getBody(first, second)
-  )
+  const content = new helper.Content('text/plain', getBody(first, second));
 
-  mail.addContent(content)
+  mail.addContent(content);
 
-  personalization = new helper.Personalization()
-  personalization.addTo(getEmail(first))
+  personalization = new helper.Personalization();
+  personalization.addTo(getEmail(first));
 
   if (second) {
-    personalization.addTo(getEmail(second))
+    personalization.addTo(getEmail(second));
   }
 
-  mail.addPersonalization(personalization)
+  mail.addPersonalization(personalization);
 
-  return mail
+  return mail;
 }
 
 function sendEmail(from, first, second) {
-  const mail = createEmail(from, first, second)
+  const mail = createEmail(from, first, second);
   const sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
   const request = sg.emptyRequest({
     method: 'POST',
     path: '/v3/mail/send',
-    body: mail.toJSON()
+    body: mail.toJSON(),
   });
 
   sg.API(request, function(error, response) {
-    console.log(response.statusCode, response.body, response.headers)
+    console.log(response.statusCode, response.body, response.headers);
     if (error) {
-      console.log(error)
+      console.log(error);
     }
-  })
+  });
 }
-
 
 const subject = `Weekly Mixer`;
 
 function getBody(first, second) {
   if (!second) {
-    const firstLine = `Hey ${first}, this week you have not been paired with anyone.`
-    const secondLine = `Feel free to treat this week as a wild card week and schedule a call with anyone you would like.`
+    const firstLine = `Hey ${first}, this week you have not been paired with anyone.`;
+    const secondLine = `Feel free to treat this week as a wild card week and schedule a call with anyone you would like.`;
     return `${firstLine}\n\n${secondLine}`;
   }
 
   const firstLine = `Hey ${first} ${second},`;
   const secondLine = `You've been paired this week! Feel free to reach out and schedule a time to talk.`;
-  return `${firstLine}\n\n${secondLine}`
+  return `${firstLine}\n\n${secondLine}`;
 }
 
+const list = shuffle(Object.keys(people), {copy: true});
+let firstHalf = list.slice(0, list.length / 2 + 1);
+let secondHalf = list.slice(list.length / 2 + 1);
 
-const { people, schedule } = yaml.load("schedule.yaml");
-const week = schedule["week 8"]
+const pairs = zip(firstHalf, secondHalf);
 
-week.forEach(pair => {
-  if (pair.includes("\t")) {
-    const [first, second] = pair.split("\t")
-    sendEmail("Jason", first, second)
-  } else {
-    sendEmail("Jason", pair)
-  }
-})
+pairs.forEach(([first, second]) => {
+  sendEmail('Jason', first, second);
+});
